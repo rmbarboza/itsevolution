5d42625097e48dbd01fc26b497a614a0234464b4
hcaixeiro <hcaixeiro@gmail.com>
2017-11-20 00:32:47 -0200

    Improvements in population generation and control

--- ItsEvolution/common/population.py
+++ ItsEvolution/common/population.py
@@ -3,11 +3,23 @@ import random
 REP_NORMALIZE = 0.5
 INC_AGE = 0.25
 
-class human:
-    def __init__(self,age):
+class Human:
+
+    def __init__(self,pop,parent1,parent2,age):
+        self.id = pop.idControl
         self.name = 'xxx'
         self.sex = random.randrange(2)
         self.age = age
+        self.cooldown = 0
+        if parent1 == None or parent2 == None:
+            self.father = None
+            self.mother = None
+        elif parent1.sex == 0:
+            self.father = parent1
+            self.mother = parent2
+        else:
+            self.father = parent2
+            self.mother = parent1
 
         r = random.randrange(1000000)
         if (r == 0):
@@ -17,6 +29,8 @@ class human:
             if(self.rep > 1):
                 self.rep = 1
 
+        pop.incIdControl()
+
     def incAge(self):
         self.age += INC_AGE
 
@@ -24,16 +38,43 @@ class human:
         if self.age > 199:
             return 1
         if self.sex == 0:
-            '''if r <= pops[int(round(self.age))].m:
-                print(int(round(self.age)))
-                print(r, pops[int(round(self.age))].m)
-                return 1'''
             return r <= pops[int(round(self.age))].m
         else:
             return r <= pops[int(round(self.age))].f
 
+    def mayIFuck(self):
+        if self.sex == 1:
+            if self.cooldown > 0:
+                self.decCoolDown()
+                return 0
+            if(self.age > 12 and self.age < 60):
+                return 1
+        else:
+            if(self.age > 12):
+                return 1
+        return 0
+
+    def potentialPartner(self,partner):
+        if(self == partner):
+            return 0
+        if(self.sex == partner.sex):
+            return 0
+        if(not partner.mayIFuck()):
+            return 0
+        return 1
+
+    def copulated(self,partner):
+        p = (self.rep + partner.rep) / 2
+        if random.random() <= p:
+            self.cooldown = 3
+            return 1
+
+    def decCoolDown(self):
+        self.cooldown -= 1
+        return 0
+
     def printAll(self):
-        print(self.name )
+        print("Id: {}".format(self.id) )
         print('--------------')
 
         if (self.sex == 0):
@@ -44,3 +85,11 @@ class human:
         print("Reproductiveness: " + str(self.rep))
 
         print('\n')
+
+
+class PopulationStats:
+    def __init__(self):
+        self.idControl = 0
+
+    def incIdControl(self):
+        self.idControl += 1
--- ItsEvolution/itsevol.py
+++ ItsEvolution/itsevol.py
@@ -1,8 +1,8 @@
 import random
-from common.population import human
-from common.utils import popstats,startpops
+from ItsEvolution.common.population import Human,PopulationStats
+from ItsEvolution.common.utils import Popcontrol,startpops
 
-YEARS = 8
+YEARS = 10
 INITIAL_POP = 100
 SEX_RAND = 0.01
 NUMBER_SEX_TRIES = int(round(SEX_RAND * INITIAL_POP))
@@ -11,18 +11,31 @@ NUMBER_SEX_TRIES = int(round(SEX_RAND * INITIAL_POP))
 hlist = []
 men = 0
 women = 0
+men_fuckable = 0
+women_fuckable = 0
 
 pops = startpops()
 
+popSt = PopulationStats()
+
 for i in range(INITIAL_POP):
-    h = human(random.randrange(0,100))
+    h = Human(popSt, None, None, random.randrange(0, 100))
     hlist.append(h)
     #if h.rep == 0:
     #   h.printAll()
     if h.sex == 0:
         men += 1
+        men_fuckable += h.mayIFuck()
     else:
         women += 1
+        women_fuckable += h.mayIFuck()
+
+print("Initial population:")
+print("-------------------")
+print("Men: {} Fuckbable: {} ({}%)".format(men,men_fuckable,round((men_fuckable/men)*100,2)))
+print("Women: {} Fuckbable: {} ({}%)".format(women,women_fuckable,round((women_fuckable/women)*100,2)))
+print("-------------------\n")
+
 
 new_hlist = []
 
@@ -31,27 +44,27 @@ born_men = born_women = 0
 
 for k in range(YEARS*4):
     for h in hlist:
-        if (h.sex == 1 and h.age < 60) or h.age > 12: #condição transante
+        if h.mayIFuck(): #condição transante
             for i in range(NUMBER_SEX_TRIES): #loop de tentativas com n indivíduos
                 n = random.randrange(hlist.__len__())
                 s = hlist[n]
-                if s.age < 12 or s == h or s.sex == h.sex:
+
+                if(not h.potentialPartner(s)):
                     continue
 
-                if s.age >= 60 and s.sex == 1:
-                    continue
-
-                p = (s.rep + h.rep)/2
-                if random.random() <= p:
-                    n = human(0)
+                if h.copulated(s):
+                    n = Human(popSt, h, s, 0)
                     new_hlist.append(n)
                     if n.sex == 0:
                         born_men += 1
                     else:
                         born_women += 1
 
+                    break
+
+
         if h.isDead(random.random(),pops): #verifica se morreu
-            #h.printAll()
+            h.printAll()
             hlist.remove(h)
             if h.sex == 0:
                 deaths_men += 1
@@ -59,7 +72,8 @@ for k in range(YEARS*4):
                 deaths_women += 1
         else:
             h.incAge()
-    hlist = hlist + new_hlist
+
+hlist = hlist + new_hlist
 
 men = men + born_men - deaths_men
 women = women + born_women - deaths_women
@@ -70,4 +84,4 @@ print("Women: " + str(women))
 print("Births: M -> " + str(born_men) + " F -> " + str(born_women))
 print("Deaths: M -> " + str(deaths_men) + " F -> " + str(deaths_women))
 
-print(pops[0].m)
+
